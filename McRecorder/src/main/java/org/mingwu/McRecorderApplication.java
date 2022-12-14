6f6e7297fcc94f5ad4317fb42ae4b14746264981
DESKTOP-5J7K06J\57m <qaz66223@gmail.com>
2022-12-14 23:37:21 +0800

    init line bot message handler

--- McRecorder/src/main/java/org/mingwu/McRecorderApplication.java
+++ McRecorder/src/main/java/org/mingwu/McRecorderApplication.java
@@ -1,5 +1,11 @@
 package org.mingwu;
 
+import com.linecorp.bot.model.event.Event;
+import com.linecorp.bot.model.event.MessageEvent;
+import com.linecorp.bot.model.event.message.TextMessageContent;
+import com.linecorp.bot.model.message.Message;
+import com.linecorp.bot.model.message.TextMessage;
+import com.linecorp.bot.spring.boot.annotation.EventMapping;
 import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
@@ -12,4 +18,23 @@ public class McRecorderApplication {
     public static void main(String[] args) {
         SpringApplication.run(McRecorderApplication.class, args);
     }
+
+    @EventMapping
+    public Message handleTextMessageEvent(MessageEvent<TextMessageContent> event) {
+        log.info("event: " + event);
+        switch(event.getMessage().getText()){
+            case "1":
+                return new TextMessage("我是1");
+            case "2":
+                return new TextMessage("我是2");
+            default:
+                return new TextMessage("覺得自己是0");
+        }
+    }
+
+    @EventMapping
+    public void handleDefaultMessageEvent(Event event) {
+        System.out.println("event: " + event);
+    }
+
 }
