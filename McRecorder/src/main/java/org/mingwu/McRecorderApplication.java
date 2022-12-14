019a6f0e5a92314b238ae3e66ad41cfb284f67cd
DESKTOP-5J7K06J\57m <qaz66223@gmail.com>
2022-12-14 23:28:07 +0800

    add dependency "lombok"、"line-bot"

--- McRecorder/src/main/java/org/mingwu/McRecorderApplication.java
+++ McRecorder/src/main/java/org/mingwu/McRecorderApplication.java
@@ -1,9 +1,13 @@
 package org.mingwu;
 
+import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@LineMessageHandler
+@Slf4j
 public class McRecorderApplication {
     public static void main(String[] args) {
         SpringApplication.run(McRecorderApplication.class, args);
